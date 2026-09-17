2597a329f282007a7a88056f7db034c199441182
xojimurodov754 <xojimurodovjaloliddi67@gmail.com>
2026-09-17 16:19:37 +0500

    i18n: kategoriya nomlari, sanalardagi oy nomlari va meta-description ham tanlangan tilga to'liq tarjima qilinadi

--- apps/items/templatetags/item_extras.py
+++ apps/items/templatetags/item_extras.py
@@ -1,14 +1,28 @@
 from django import template
+from django.utils.translation import gettext as _
 
 register = template.Library()
+
+_MONTH_IDS = {
+    1: "yanvar",
+    2: "fevral",
+    3: "mart",
+    4: "aprel",
+    5: "may",
+    6: "iyun",
+    7: "iyul",
+    8: "avgust",
+    9: "sentabr",
+    10: "oktabr",
+    11: "noyabr",
+    12: "dekabr",
+}
 
 
 @register.filter
 def format_date(value):
-    """Sana: '17-sentabr, 2026' ko'rinishida chiqaradi."""
-    months_uz = {
-        1: "yanvar", 2: "fevral", 3: "mart", 4: "aprel",
-        5: "may", 6: "iyun", 7: "iyul", 8: "avgust",
-        9: "sentabr", 10: "oktabr", 11: "noyabr", 12: "dekabr",
-    }
-    return f"{value.day}-{months_uz[value.month]}, {value.year}"
+    """Sana oy nomini har bir so'rovda faol tilga moslab chiqaradi: '17-sentabr, 2026'."""
+    if value is None:
+        return ""
+    month = _(_MONTH_IDS.get(value.month, value.month))
+    return f"{value.day}-{month}, {value.year}"
